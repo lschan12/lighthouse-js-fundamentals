f20bb5b6557d835fa507d46dad95ec1698f8c3f1
Lawrence Chan <lawrence.c@hotmail.co.uk>
2022-05-23 13:11:26 -0700

    using functions to print different names

--- chorus-while.js
+++ chorus-while.js
@@ -1,5 +1,6 @@
 const chorus = "Let's dance!";
 let repeat = 0;
+
 while (repeat < 10) {
   if (repeat === 5){
     console.log("*change key*");
@@ -8,4 +9,3 @@ while (repeat < 10) {
   repeat++;
 }
 console.log("Until the sun comes up!");
-
--- conditionals.js
+++ conditionals.js
@@ -26,7 +26,7 @@ if (temperature < 0) {
 }
 
 console.log("Now you're ready to go outside!");
-*/
+
 
 const isCitizen = true;
 const age = 26;
@@ -46,4 +46,24 @@ const raining = false;
 if (!raining) {
   console.log("Leave your umbrella at home!");
 }
+
+*/
+
+function whichSchool(age){
+  if (age < 13) {
+    return "Elementary School";
+  } else if (age >= 13 && age <= 18){
+    return "Secondary School";
+  } else {
+    return "Lighthouse Labs";
+  }
+}
+
+console.log("I am 35. Which school should I go to?");
+console.log(whichSchool(35));
+console.log("I am 8. Which school should I go to?");
+console.log(whichSchool(8));
+console.log("I am 14. Which school should I go to?");
+console.log(whichSchool(14));
+
 
